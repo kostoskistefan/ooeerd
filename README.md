a4aaa5f5ade93e57fb2ac22ed5a165cfeeac90dd
Kostoski Stefan <kostoski.stefan90@gmail.com>
2023-06-12 19:28:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Ooeerd
+
+Ooeerd (pronounced /ˈwɪɹd/) is a useless piece of software, derived from an inside joke.
+
+The main purpose of Ooeerd is to replace certain letters in a given text, based on a [substitution model](#substitution-model).
+
+Access the software at: [kostoskistefan.github.io/ooeerd](https://kostoskistefan.github.io/ooeerd)
+
+### Substitution model
+
+|              Letter            | Substitution |       Mode       |
+| ------------------------------ | ------------ | ---------------- |
+| u                              | oo           | Classic/Hardcore |
+| i                              | ee           | Classic/Hardcore |
+| w                              | oo           | Hardcore only    |
+| y (at the beginning of a word) | ee           | Hardcore only    |
---
+++ docs/main.js
@@ -0,0 +1,39 @@
+window.onload = start_ooeerd;
+
+let mode_select = document.getElementById("mode");
+let input_textarea = document.getElementById("input-textarea");
+let output_textarea = document.getElementById("output-textarea");
+
+function start_ooeerd()
+{
+    mode_select.addEventListener('change', process_text);
+    input_textarea.addEventListener('input', process_text);
+}
+
+function process_text(e)
+{
+    let text = input_textarea.value;
+
+    text = text.replace(/i/g, "ee");
+    text = text.replace(/I([a-z ])/g, "Ee$1");
+    text = text.replace(/I([A-Z])/g, "EE$1");
+
+    text = text.replace(/u/g, "oo");
+    text = text.replace(/U([a-z ])/g, "Oo$1");
+    text = text.replace(/U([A-Z])/g, "OO$1");
+
+    let mode = mode_select.value;
+
+    if (mode === "hardcore")
+    {
+        text = text.replace(/w/g, "oo");
+        text = text.replace(/W([a-z ])/g, "Oo$1");
+        text = text.replace(/W([A-Z])/g, "OO$1");
+
+        text = text.replace(/(^|\W)y/g, "ee");
+        text = text.replace(/(^|\W)Y([a-z ])/g, "Ee$1");
+        text = text.replace(/(^|\W)Y([A-Z])/g, "EE$1");
+    }
+
+    output_textarea.value = text;
+}
